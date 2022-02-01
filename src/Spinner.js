8958c301fee6717d25927fe613630b6a5530d966
jdaublas <87877758+jdaublas@users.noreply.github.com>
2022-02-01 12:53:20 -0700

    changed spinner color to gold

--- src/Spinner.js
+++ src/Spinner.js
@@ -2,5 +2,5 @@ import React from "react";
 import { Hearts } from "react-loader-spinner";
 
 export default function Spinner() {
-  return <Hearts heigth="100" width="100" color="red" ariaLabel="loading" />;
+  return <Hearts heigth="100" width="100" color="gold" ariaLabel="loading" />;
 }
